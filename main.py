16e95cc55b6eba7a91176782793c28f6f91809da
Roman Sokolov <gnonasis@gmail.com>
2025-09-20 10:04:56 +0300

    add on_startup, remove_author, remove_book fix create_author,

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, Query, Path, HTTPException
+from fastapi.responses import Response
 from sqlalchemy.orm import Session
 
 from crud import (
@@ -6,12 +7,18 @@ from crud import (
     get_author_by_name,
     get_author_by_id,
     add_author_to_db,
-    get_all_books, add_book_to_db
+    get_all_books, add_book_to_db, delete_book, delete_author
 )
-from database import SessionLocal
+from database import SessionLocal, engine
+from models import Base
 from schemas import AuthorRead, AuthorCreate, BookRead, BookCreate
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
 
 def get_db() -> Session:  # type: ignore
     db = SessionLocal()
@@ -32,11 +39,7 @@ def list_authors(
 def create_author(author: AuthorCreate, db: Session = Depends(get_db)):
     db_author = get_author_by_name(db=db, author_name=author.name)
     if db_author is None:
-        db_author = add_author_to_db(author=author, db=db)
-        db.add(db_author)
-        db.commit()
-        db.refresh(db_author)
-        return db_author
+        return add_author_to_db(author=author, db=db)
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Author already exists"
@@ -51,6 +54,11 @@ def get_author(author_id: int = Path(ge=1), db: Session = Depends(get_db)):
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Author not found"
     )
+
+@app.delete("/authors/{author_id}", status_code=status.HTTP_204_NO_CONTENT)
+def remove_author(author_id: int, db: Session = Depends(get_db)):
+    delete_author(db=db, author_id=author_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.get("/books", response_model=list[BookRead])
 def list_books(
@@ -68,4 +76,15 @@ def list_books(
 
 @app.post("/books", response_model=BookRead)
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    return add_book_to_db(book=book, db=db)
+    author = get_author_by_id(db=db, author_id=book.author_id)
+    if author is None:
+        return add_book_to_db(book=book, db=db)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Author not found"
+    )
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+def remove_book(book_id: int, db: Session = Depends(get_db)):
+    delete_book(db=db, book_id=book_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
